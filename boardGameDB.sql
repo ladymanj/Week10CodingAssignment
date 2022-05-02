23d10f7d027014609377e31b9f0ee7267c302d34
Jordan Ladyman <jordan.ladyman@outlook.com>
2022-05-01 21:25:55 -0500

    Update boardGameDB.sql
    Fixed spacing issue on line 8

--- boardGameDB.sql
+++ boardGameDB.sql
@@ -5,7 +5,7 @@ USE board_games;
 DROP TABLE IF EXISTS board_games;
 
 CREATE TABLE board_games (
-		id int(11) NOT NULL AUTO_INCREMENT,
+	id int(11) NOT NULL AUTO_INCREMENT,
         name varchar(50) NOT NULL,
         min_player_count int(3) NOT NULL,
         max_player_count int(3) NOT NULL,
